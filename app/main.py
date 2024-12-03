d3ab304adb36cc086d245005c605125ba936f280
Steffany-Campo <steffani.campo@gmail.com>
2024-12-03 12:20:52 -0500

    Se actualiza el archivo main del proyecto app con la implementación del módulo Pandas

--- app/main.py
+++ app/main.py
@@ -1,23 +1,29 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 #print(keys, values)
 
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  '''
   data = list(filter(lambda cont: cont['Continent'] == 'South America', data))
   #list_porcentage = list(map(lambda porc: porc['World Population Percentage'], data))
   #list_countrys = list(map(lambda country: country['Country/Territory'], data))
-  
   countrys = utils.popul_countrys(data)
   percentages = utils.popul_perce(data)
+  '''
 
-  #print(countrys, percentages)
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'South America']
 
-  charts.generate_pie_chart(countrys, percentages)
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
   
+  
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   result = utils.population_by_country(data, country)
   if len(result) > 0:
