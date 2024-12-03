f7ec8e67908a97b56e962d9e44c6641ee70a839b
Steffany-Campo <steffani.campo@gmail.com>
2024-12-03 12:22:09 -0500

    Actualización del proyecto de Web-Server usando FastAPI y el servidor Web Uvicorn. Ha trabajado correctamente en la web

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list ():
+    return """
+    <html>
+        <head>
+            <title>Bienvenidos</title>
+        </head>
+        <body>
+            <h1>Soy un título</h1>
+            <p> Esto es un párrafo </p>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
